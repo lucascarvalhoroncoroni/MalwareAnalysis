307cd39c4b73db4233fc6d7ec5529074332ee118
lucascarvalhoroncoroni <lucascarvalhoroncoroni@gmail.com>
2017-04-30 23:23:35 -0300

    upload simples de arquivo sem guardar na base de dados

--- mainProject/blog/admin.py
+++ mainProject/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import posts
+
 # Register your models here.
+
+admin.site.register(posts)
--- mainProject/mainProject/settings.py
+++ mainProject/mainProject/settings.py
@@ -82,7 +82,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
